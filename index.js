b173cd531fa8f89bb5f96a5fc005f5ffe43e940b
VanamaTharunKumar <tharunkumarvanama@gmail.com>
2026-01-24 11:44:39 +0000

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Windows EC2 via CI/CD ");
+  res.send("Hello from Windows EC2 via CI/CD(POC-2) ");
 });
 
 app.listen(3000, () => {
